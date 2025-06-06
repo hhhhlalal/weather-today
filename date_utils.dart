36b0e6a88491a4f514674c88281999c0aab20da5
LHThai <59799035+hhhhlalal@users.noreply.github.com>
2025-06-06 09:56:25 +0700

    Delete date_utils.dart

--- date_utils.dart
+++
@@ -1,8 +0,0 @@
-String getLunarDateStr(DateTime date) {
-  final lunarCycle = 29.53;
-  final diff = date.difference(DateTime(1900, 1, 31));
-  final days = diff.inDays % lunarCycle;
-  final lunarDay = (days + 1).round();
-  final lunarMonth = ((date.month + 1) % 12) + 1;
-  return 'Âm: $lunarDay/$lunarMonth';
-}
